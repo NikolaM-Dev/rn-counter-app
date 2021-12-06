7df4fbbf5638f9078f13cc371965e891f1e24a83
Juan David Merchan Torres <juan1014166@gmail.com>
2021-12-06 10:28:40 -0500

    feat(screens): Add dimensions

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import {SafeAreaView} from 'react-native';
 
 // import HelloWorldScreen from './src/screens/HelloWorldScreen';
-import CounterScreen from './src/screens/CounterScreen';
+// import CounterScreen from './src/screens/CounterScreen';
+// import BoxObjectModelScreen from './src/screens/BoxObjectModelScreen';
+import DimensionsScreen from './src/screens/DimensionsScreen';
 
 const App = () => {
   return (
     // <HelloWorldScreen />;
-    <CounterScreen />
+    // <CounterScreen />
+    <SafeAreaView style={{flex: 1}}>
+      {/* <BoxObjectModelScreen /> */}
+      <DimensionsScreen />
+    </SafeAreaView>
   );
 };
 
---
+++ src/screens/DimensionsScreen.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  useWindowDimensions,
+} from 'react-native';
+
+// const {width, height} = Dimensions.get('window');
+
+const DimensionsScreen = () => {
+  const {width, height} = useWindowDimensions();
+
+  return (
+    <View>
+      <View style={styles.container}>
+        <View
+          style={{
+            ...styles.purpleBox,
+            width: width * 2,
+          }}
+        />
+        <View style={styles.orangeBox} />
+      </View>
+      <Text style={styles.title}>
+        Width: {width}, Height: {height}
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'red',
+    width: '100%',
+    height: 200,
+  },
+  purpleBox: {
+    backgroundColor: '#5856D6',
+    // width: '50%',
+    height: '50%',
+  },
+  orangeBox: {
+    backgroundColor: '#F0A23B',
+  },
+  title: {
+    fontSize: 30,
+  },
+});
+
+export default DimensionsScreen;
