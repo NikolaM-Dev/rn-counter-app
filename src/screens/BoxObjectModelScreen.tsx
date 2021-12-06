dd7a125c9392ff3807221d5d2427197ad1ec9914
Juan David Merchan Torres <juan1014166@gmail.com>
2021-12-06 10:28:29 -0500

    feat(screens): Add box object model

---
+++ src/screens/BoxObjectModelScreen.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const BoxObjectModelScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Box Object Model</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+  title: {
+    fontSize: 20,
+    // width: 150,
+    borderWidth: 10,
+    // backgroundColor: 'green',
+  },
+});
+
+export default BoxObjectModelScreen;
